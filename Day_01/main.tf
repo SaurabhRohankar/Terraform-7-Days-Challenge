5d621e762fe2963bf9982ce726bc0b9dd740915a
Saurabh <saurabhrohankar99@gmail.com>
2023-10-10 09:32:22 +0530

    Fixed user data

--- Day_01/main.tf
+++ Day_01/main.tf
@@ -1,63 +1,69 @@
 provider "aws" {
-    region = "ap-south-1"
+  region = "ap-south-1"
 }
 
-#retrieving default vpc id
+# Retrieving default VPC ID
 data "aws_vpc" "default" {
   default = true
 }
 
-
-#launching ec2 instance
+# Launching EC2 instance
 resource "aws_instance" "tf-ec2" {
-    ami = "ami-0c42696027a8ede58"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-1e6e6776"
-    security_groups = [aws_security_group.allow-http-sg.id]
-    key_name = "new-key"
-   
-#using file function to read user data script
-
-    user_data = <<EOF
+  ami           = "ami-0c42696027a8ede58"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-1e6e6776"
+  key_name      = "new-key"
+  vpc_security_group_ids = [aws_security_group.allow-http-ssh-sg.id]
+  
+  # Using file function to read user data script
+  user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
     sudo yum install httpd -y
-    sudo systemctl start htttp -y
+    sudo systemctl start httpd
     sudo systemctl enable httpd
-    echo "Hi from $(hostname -f)" > /var/www/html/index.html
-    EOF
+    echo "Hello from $(hostname -f)" > /var/www/html/index.html
+  EOF
 
-    tags =  {
-        Name = "TF-EC2"
-        Env = "Dev"
-    }
+  tags = {
+    Name = "TF-EC2"
+    Env  = "Dev"
+  }
 }
 
-#SG for ec2 instance
-resource "aws_security_group" "allow-http-sg" {
-    name = "TF-allow-http-sg"
-    description = "SG made by TF"
-    vpc_id = data.aws_vpc.default.id
+# Security group for EC2 instance
+resource "aws_security_group" "allow-http-ssh-sg" {
+  name        = "TF-allow-http-ssh-sg"
+  description = "SG made by TF"
+  vpc_id      = data.aws_vpc.default.id
 
-    ingress {
-        description = "http from anywhere"
-        from_port = 80
-        to_port = 80
-        cidr_blocks =  ["0.0.0.0/0"]
-        protocol = "tcp"
-    }
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        description = "For SSH"
-        from_port = 22
-        to_port = 22
-        cidr_blocks = ["0.0.0.0/0"]
-        protocol = "tcp"
-    }
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with your trusted IP address
+  }
 
-    tags = {
-        Name = "TF-allow-http-sg"
-    }
+  # Outbound rules - allow all outbound traffic by default
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "TF-allow-http-ssh-sg"
+  }
 }
 
 output "vpc_id" {
@@ -67,3 +73,4 @@ output "vpc_id" {
 output "public_ip" {
   value = aws_instance.tf-ec2.public_ip
 }
+

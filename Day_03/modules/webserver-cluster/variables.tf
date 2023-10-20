bff65cd61772e15877c2e8fa9bcad247b093e19e
Saurabh <saurabhrohankar99@gmail.com>
2023-10-20 09:52:14 +0530

    Converted Webserver cluster into module

---
+++ Day_03/modules/webserver-cluster/variables.tf
@@ -0,0 +1,35 @@
+variable "ami_id_value" {
+  description = "AMI ID of ec2 instance"
+  type        = string
+}
+
+variable "instance_type_value" {
+  description = "Instance type of ec2"
+  type        = string
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+  type        = number
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+  type        = number
+}
+
+variable "desired_size" {
+  description = "The desired number of EC2 Instances in the ASG"
+  type        = number
+}
+
+variable "key_pair_name_value" {
+  description = "Key pair for ssh into EC2"
+  type        = string
+}
+
+
+variable "greeter_name" {
+  type        = string
+  description = "Person who will greet user on website"
+}
